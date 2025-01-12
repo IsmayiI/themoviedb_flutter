2d1d79180b3de8c5702a6cc61edfc7ff97b68f2f
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-01-13 01:22:54 +0400

    add cast info

--- lib/ui/widgets/movie_details/movie_details_cast_info_widget.dart
+++ lib/ui/widgets/movie_details/movie_details_cast_info_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:themoviedb_flutter/assets/images.dart';
+import 'package:themoviedb_flutter/domain/api_client/api_service.dart';
+import 'package:themoviedb_flutter/provider/provider.dart';
+import 'package:themoviedb_flutter/ui/widgets/movie_details/movie_details_model.dart';
 import 'package:themoviedb_flutter/ui/widgets/theme/app_colors.dart';
 
 class MovieDetailsCastInfoWidget extends StatelessWidget {
@@ -7,95 +10,182 @@ class MovieDetailsCastInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final castList =
+        NotifierProvider.watch<MovieDetailsModel>(context).movieCredits?.cast;
+
+    if (castList == null) return const SizedBox.shrink();
+
+    final cast = castList.length > 9 ? castList.sublist(0, 9) : castList;
+
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text('Top Billed Cast',
-                style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600)),
-          ),
+          const _TitleWidget(),
           SizedBox(
-            height: 250,
+            height: 246,
             child: Scrollbar(
               child: ListView.builder(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  itemCount: 10,
+                  itemCount: cast.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (BuildContext context, int index) {
-                    return Row(
-                      children: [
-                        Container(
-                          width: 120,
-                          clipBehavior: Clip.hardEdge,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(color: AppColors.lightGrey),
-                            borderRadius: BorderRadius.circular(6),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color.fromRGBO(0, 0, 0, .1),
-                                offset: Offset(0, 2),
-                                blurRadius: 8,
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Image(
-                                width: 120,
-                                height: 133,
-                                image: AppImages.rosemaryActor,
-                                fit: BoxFit.cover,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Text(
-                                      'Mia Farrow',
-                                      textAlign: TextAlign.center,
-                                      maxLines: 2,
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w700),
-                                    ),
-                                    Text(
-                                      'Rosemary Woodhouse',
-                                      textAlign: TextAlign.center,
-                                      maxLines: 2,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        if (index != 9) SizedBox(width: 10),
-                      ],
-                    );
+                    return _CastItemWidget(index);
                   }),
             ),
           ),
           SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextButton(
-              onPressed: () {},
-              child: Text('Full Cast & Crew',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600)),
-            ),
-          ),
+          const _SecondTitleWidget(),
         ],
+      ),
+    );
+  }
+}
+
+class _CastItemWidget extends StatelessWidget {
+  const _CastItemWidget(this.index);
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 120,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: AppColors.lightGrey),
+            borderRadius: BorderRadius.circular(6),
+            boxShadow: [
+              const BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, .1),
+                offset: Offset(0, 2),
+                blurRadius: 8,
+              ),
+            ],
+          ),
+          child: _PersonWidget(index),
+        ),
+        if (index != 8) SizedBox(width: 10),
+      ],
+    );
+  }
+}
+
+class _PersonWidget extends StatelessWidget {
+  final int index;
+  const _PersonWidget(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _ProfileImgWidget(index),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: _NameWidget(index),
+        ),
+      ],
+    );
+  }
+}
+
+class _NameWidget extends StatelessWidget {
+  final int index;
+  const _NameWidget(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    final castList =
+        NotifierProvider.watch<MovieDetailsModel>(context).movieCredits?.cast;
+
+    if (castList == null) return const SizedBox.shrink();
+
+    final cast = castList.length > 9 ? castList.sublist(0, 9) : castList;
+
+    final person = cast[index];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          person.name,
+          maxLines: 2,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+        ),
+        Text(
+          person.character,
+          maxLines: 2,
+        ),
+      ],
+    );
+  }
+}
+
+class _ProfileImgWidget extends StatelessWidget {
+  final int index;
+  const _ProfileImgWidget(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    final castList =
+        NotifierProvider.watch<MovieDetailsModel>(context).movieCredits?.cast;
+
+    if (castList == null) return const SizedBox.shrink();
+
+    final cast = castList.length > 9 ? castList.sublist(0, 9) : castList;
+
+    final profilePath = cast[index].profilePath;
+
+    if (profilePath == null) {
+      return Container(
+        width: 120,
+        height: 133,
+        color: AppColors.lightGrey,
+        child: Icon(
+          Icons.person,
+          size: 120,
+          color: Color(0xFFB5B5B5),
+        ),
+      );
+    }
+
+    return Image.network(
+      ApiService.imageUrl(profilePath),
+      width: 120,
+      height: 133,
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class _TitleWidget extends StatelessWidget {
+  const _TitleWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Top Billed Cast',
+        style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600));
+  }
+}
+
+class _SecondTitleWidget extends StatelessWidget {
+  const _SecondTitleWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextButton(
+      onPressed: null,
+      child: Text(
+        'Full Cast & Crew',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
